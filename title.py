22b2215164821b40144bffebeeea82264915904e
Anthony Alves <aalves2012@my.fit.edu>
2016-03-08 14:43:17 -0500

    added custom title for YouTube and Registered User checks

--- title.py
+++ title.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8
 
-# Copyright (C) 2013 aciid<github>
+# Copyright (C) 2013 Anthony Alves <cvballa3g0@gmail.com>
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -34,14 +34,22 @@
 #
 
 from mumo_module import (commaSeperatedIntegers,
+                         commaSeperatedBool,
                          MumoModule)
 
-from datetime import timedelta
-import urllib, base64, re, BeautifulSoup
+import urllib2, re, BeautifulSoup
+
 
 
 
 class title(MumoModule):
+    default_config = {'title':(
+                                ('servers', commaSeperatedIntegers, []),
+                                ),
+                                lambda x: re.match('(all)|(server_\d+)', x):(
+                                    ('reactonregisteredonly', commaSeperatedBool, [True]),
+                                )
+                    }
 
     def __init__(self, name, manager, configuration = None):
         MumoModule.__init__(self, name, manager, configuration)
@@ -49,31 +57,76 @@ class title(MumoModule):
 
     def connected(self):
         manager = self.manager()
-        log = self.log()
-        log.debug("Register for Server callbacks")
+        self.log().debug("Register [%s] callbacks", self.name())
 
-        servers = manager.SERVERS_ALL
-
-        manager.subscribeServerCallbacks(self, servers)
+        manager.subscribeServerCallbacks(self, self.cfg().title.servers or manager.SERVERS_ALL)
 
         
     def disconnected(self): pass
 
     def sendMessage(self, server, user, message, msg):
-        server.sendMessageChannel(user.channel, False, msg)
+        if message.channels: # sent to a channel
+            server.sendMessageChannel(user.channel, False, msg)
+        else: # sent as a private message
+            server.sendMessage(message.sessions[0], msg)
+            
+    def getYouTubeTitle(self, id):
+        endpoint = 'http://youtube.com/get_video_info?video_id=' + id
+        sock = urllib2.urlopen(endpoint)
+        
+        # get the file contents
+        videostr = sock.read()
+        sock.close()
+        
+        # parse the text for channel and title
+        tagRegex = '&tag=([^\n^\r^&]*)'
+        channel = re.findall(tagRegex.replace("tag", "author"), videostr)[0]
+        title = re.findall(tagRegex.replace("tag", "title"), videostr)[0]
+        
+        # clean up the text
+        channel = urllib2.unquote(channel).replace("+", " ")
+        title = urllib2.unquote(title).replace("+", " ")
+        
+        
+        # create the server message
+        return channel + " :: " + title
+                
+   
     #
     #--- Server callback functions
     #
-
     def userTextMessage(self, server, user, message, current=None):
+    
+            sid = server.id()
+            try:
+                scfg = getattr(self.cfg(), 'server_%d' % sid)
+            except AttributeError:
+                scfg = self.cfg().all
+
+            # check if the user is registered
+            if scfg.reactonregisteredonly and user.userid == -1:
+                self.log().debug('\'reactonregisteredonly\' is enabled; Ignored unregistered user \'%s\' (session id \'%s\')' % (user.name, user.session))
+                return
+                
+            
             msg = message.text
             link = re.findall(r'href=[\'"]?([^\'" >]+)', msg)
-            print link[0]
-            soup = BeautifulSoup.BeautifulSoup(urllib.urlopen(link[0]))
-            print soup.title.string
-            self.sendMessage(server, user, message, soup.title.string)
-            return
+            
+            if len(link) < 1:
+                # url was not included in this message
+                return
+                
+            # check if it is a YouTube link
+            idRegex = '(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})'
+            videoId = re.findall(idRegex, msg)
 
+            if len(videoId) >= 1: # YouTube link
+                # not a YouTube link
+                msg = self.getYouTubeTitle(videoId[0])
+            else:
+                msg = BeautifulSoup.BeautifulSoup(urllib2.urlopen(link[0])).title.string
+                
+            self.sendMessage(server, user, message, msg)
 
 
 
